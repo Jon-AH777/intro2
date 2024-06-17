03537871a3dea6427e9c08b4ffa97be0e826c0c9
Jona Subba <sanarung.subba@gmail.com>
2024-06-17 08:56:44 +0545

    added some elements

--- src/app/news/[id]/page.js
+++ src/app/news/[id]/page.js
@@ -1,9 +1,17 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+
 export default function NewsDetailPage({params}){
-    const newsId = params.id;
+    const newsSlug = params.id;
+    const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug)
+
     return (
-        <>
-            <h1>News Detail Page</h1>
-            <p>News ID: {newsId}</p>
-        </>
+        <article className="news-article">
+            <header>
+                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                <h1>{newsItem.title}</h1>
+                <time dateTime={newsItem.date}>{newsItem.date}</time>
+            </header>
+            <p>{newsItem.content}</p>
+        </article>
     )
 }
