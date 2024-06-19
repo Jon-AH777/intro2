d59d808304a8843cd3f0239a417e1fb80d68fc85
Jona Subba <sanarung.subba@gmail.com>
2024-06-19 09:03:32 +0545

    some changes

---
+++ src/app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage(){
+    return (
+        <div id="error">
+            <h1>Not fund!</h1>
+            <p>The requested resource could not be found</p>
+        </div>
+    )
+}
